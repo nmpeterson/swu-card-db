093fc9bb2af799f828185dd79a523aceaccb3443
Noel Peterson <nmpeterson@users.noreply.github.com>
2025-01-08 15:19:17 -0600

    Remove set_id route param from card_list, allow some query params

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from typing import Annotated, Union
+import logging
+from typing import Annotated
 
 from fastapi import FastAPI, Request, Depends, Header
 from fastapi.encoders import jsonable_encoder
@@ -9,6 +10,7 @@ from sqlalchemy.orm import Session
 
 from .database import get_db, SWUSet, SWUCard
 
+logging.basicConfig(level=logging.DEBUG)
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
@@ -38,6 +40,8 @@ async def get_card(request: Request, card_id: str, db: Session = Depends(get_db)
                 SWUCard.card_type == card.card_type,
                 SWUCard.subtitle == card.subtitle,
             )
+            .join(SWUCard.card_set)
+            .order_by(SWUSet.number, SWUCard.id)
             .all()
         )
         return templates.TemplateResponse(
@@ -49,8 +53,10 @@ async def get_card(request: Request, card_id: str, db: Session = Depends(get_db)
 
 
 @app.get("/set_list", response_class=HTMLResponse)
-async def set_list(
-    request: Request, db: Session = Depends(get_db), hx_request: Annotated[Union[str, None], Header()] = None
+async def get_sets(
+    request: Request,
+    db: Session = Depends(get_db),
+    hx_request: Annotated[str | None, Header()] = None,
 ):
     sets = db.query(SWUSet).order_by(SWUSet.number).all()
     if hx_request:
@@ -58,19 +64,20 @@ async def set_list(
     return JSONResponse(content=jsonable_encoder(sets))
 
 
-@app.get("/card_list/{set_id}", response_class=HTMLResponse)
-async def card_list(
+@app.get("/card_list", response_class=HTMLResponse)
+async def get_cards(
     request: Request,
-    set_id: str,
     db: Session = Depends(get_db),
-    hx_request: Annotated[Union[str, None], Header()] = None,
+    hx_request: Annotated[str | None, Header()] = None,
 ):
-    cards = (
-        db.query(SWUCard)
-        .filter(SWUCard.set_id == set_id, SWUCard.variant_type == "Normal")
-        .order_by(SWUCard.number)
-        .all()
-    )
+    cards = db.query(SWUCard)
+    if set_id := request.query_params.get("set_id"):
+        cards = cards.filter(SWUCard.set_id == set_id)
+    if variant_type := request.query_params.get("variant_type"):
+        cards = cards.filter(SWUCard.variant_type == variant_type)
+    if card_type := request.query_params.get("card_type"):
+        cards = cards.filter(SWUCard.card_type == card_type)
+    cards = cards.join(SWUCard.card_set).order_by(SWUSet.number, SWUCard.id).all()
     if hx_request:
         return templates.TemplateResponse(request=request, name="card_list.html", context={"cards": cards})
     return JSONResponse(content=jsonable_encoder(cards))
