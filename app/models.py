9dbb4a48791028d38d14f5ea7b90555b8114cd9b
Noel Peterson <nmpeterson@users.noreply.github.com>
2025-01-08 18:14:30 -0600

    Created pydantic data models

---
+++ app/models.py
@@ -0,0 +1,43 @@
+from pydantic import BaseModel
+
+
+class SetModel(BaseModel):
+    id: str
+    name: str
+    number: int
+
+
+class ArenaModel(BaseModel):
+    arena: str | None
+
+
+class AspectModel(BaseModel):
+    aspect: str | None
+    double: bool
+
+
+class TraitModel(BaseModel):
+    trait: str | None
+
+
+class CardModel(BaseModel):
+    id: str
+    set_id: str
+    number: str
+    name: str
+    subtitle: str | None
+    unique: bool
+    rarity: str
+    variant_type: str
+    card_type: str
+    cost: str | None
+    power: str | None
+    hp: str | None
+    front_text: str | None
+    double_sided: bool
+    epic_action: str | None
+    back_text: str | None
+    artist: str
+    arenas: list[ArenaModel]
+    aspects: list[AspectModel]
+    traits: list[TraitModel]
